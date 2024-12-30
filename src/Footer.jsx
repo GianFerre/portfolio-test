24938ecc69b86bedf3a8b65244ed8104e35bdedb
Gian <gian.puse@oqulo.com>
2024-12-30 22:28:10 +0800

    Updates

--- src/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return(
-        <footer className="footer">
-            <p>&copy; {new Date().getFullYear()} Gian's Portfolio</p>
-        </footer>
-    );
-}
-
-export default Footer;
